805e60be806e87b84b0e8f3b4aec7af8c7a06298
miriamOrlansky <115842210+miriamOrlansky@users.noreply.github.com>
2024-02-03 23:07:11 -0500

    try fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -71,11 +71,11 @@ export default function Home() {
         <CaseStudy title="Fridg-It" source={FridgIt} link="/fridg-it" />
         <StudyCase title="Clef" source={Clef} link="/clef" />
         <CaseStudy title="Notee" source={Notee} link="/notee" />
-        <CaseStudy
+        {/* <CaseStudy
           title="Petite Pattiserrie"
           source={GraphicDesign}
           link="/petite"
-        />
+        /> */}
         <StudyCase
           title="Graphic Design Work"
           source={GraphicDesign}
