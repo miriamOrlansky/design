10597c2d0a16482d2644ef8df98b356f0ded07b6
miriamOrlansky <115842210+miriamOrlansky@users.noreply.github.com>
2024-02-04 23:50:10 -0500

    new back color

--- src/pages/petite.jsx
+++ src/pages/petite.jsx
@@ -14,7 +14,7 @@ import sketch4 from "../Images/sketch4.jpg";
 export default function Petite() {
   return (
     <Root>
-      <TitleSection backColor="#f2f2f2">
+      <TitleSection backColor="#eiai6a">
         <Title>Petite Patisserie</Title>
         <Subtitle>A fun way to organize your kitchen life</Subtitle>
       </TitleSection>
