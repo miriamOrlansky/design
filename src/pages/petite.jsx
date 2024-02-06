67778190bb19ee9a91f693217df6d98b02e12886
miriamOrlansky <115842210+miriamOrlansky@users.noreply.github.com>
2024-02-05 20:43:43 -0500

    smaller

--- src/pages/petite.jsx
+++ src/pages/petite.jsx
@@ -37,7 +37,7 @@ export default function Petite() {
         </div>
       </Row>
       <Row>
-        <img src={mainPetite} alt="mainPetite" />
+        <img className="third" src={mainPetite} alt="mainPetite" />
       </Row>
       <Row>
         <img className="third" src={cookieAsset} alt="cookie" />
